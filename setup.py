00551dc6942ed4c69623d7d29eaecf5fad5d3304
Linus Groschke <linus@elektronisch.dev>
2022-12-30 18:41:58 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py2n',
+    version='0.0.1',
+    author="Linus Groschke",
+    author_email="linus@elektronisch.dev",
+    description="Python library for 2N® devices",
+    license="MIT",
+    url="https://github.com/elektr0nisch/py2n",
+    packages=['py2n'],
+    keywords=['2n']
+)
